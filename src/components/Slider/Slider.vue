963ed77d2893417c9e4246957db91c13acaeff1c
x3malmusic <x3malmusic@gmail.com>
2020-09-02 17:36:11 +0300

    Component Slider done

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="slider">
+    <slot></slot>
+    <div class="slider-control">
+      <button class="slider-control-btn-left" @click="showPrev">
+        <img src="./chevron.svg" alt="left" class="arrow-left" />
+      </button>
+      <button class="slider-control-btn-right" @click="showNext">
+        <img src="./chevron.svg" alt="right" class="arrow-right" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  data: () => ({
+    currentImg: 0,
+    images: 0,
+  }),
+  methods: {
+    showNext() {
+      this.currentImg++;
+      if (this.currentImg > this.images) this.currentImg = 0;
+      this.showSlide();
+    },
+
+    showPrev() {
+      this.currentImg--;
+      if (this.currentImg < 0) this.currentImg = this.images;
+      this.showSlide();
+    },
+
+    showSlide() {
+      this.$slots.default.forEach((el, i) => {
+        if (this.currentImg != i) el.elm.className += " imageHide";
+        else
+          el.elm.className = el.elm.className
+            .split(" ")
+            .filter((e) => e != "imageHide")
+            .join(" ");
+      });
+    },
+  },
+
+  mounted() {
+    this.images = this.$slots.default.length - 1;
+    this.showSlide();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "slider";
+</style>
