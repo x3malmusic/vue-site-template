0dbdc7f07d2fd1bd4f6b97c29b451829e63d3a58
x3malmusic <x3malmusic@gmail.com>
2020-09-02 17:35:28 +0300

    Component select done

---
+++ src/components/Select/AppSelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="select-wrapper">
+    <select class="select">
+      <option value="option" v-for="option in options">{{ option }}</option>
+    </select>
+    <img class="select-arrow" src="./chevron.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppSelect",
+  props: {
+    options: Array,
+  },
+};
+</script>
+
+<style lang="scss">
+@import "select";
+</style>
