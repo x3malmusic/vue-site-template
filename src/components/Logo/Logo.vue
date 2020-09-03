1d50ea51a5264eb8278b81fd3cc333e3e30f489b
x3malmusic <x3malmusic@gmail.com>
2020-09-03 16:04:17 +0300

    Logo component added

---
+++ src/components/Logo/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="logo">
+    <router-link to="/">
+      <img src="../../assets/images/logo.png" alt="logo" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo",
+};
+</script>
+
+<style lang="scss">
+@import "logo";
+</style>
